88c871fc2b474a41638c61ae0eefef8bcdcdabac
Shivansh <shivanshnewemail@gmail.com>
2026-04-06 03:00:03 +0530

    Change model from llama3 to mixtral

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if url:
                 URL: {url}
                 """
             }],
-            model="llama3-8b-8192"
+            model="mixtral-8x7b-32768"
         )
 
         st.subheader("Summary")
